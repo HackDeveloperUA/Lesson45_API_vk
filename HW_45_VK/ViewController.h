625defb09476cedefa31470cc932e14aa6adcf3a
MD <md@MacBook-Air-MD.Dlink>
2015-08-29 21:00:53 +0300

    Initial Commit

---
+++ HW_45_VK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW_45_VK
+//
+//  Created by MD on 29.08.15.
+//  Copyright (c) 2015 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
